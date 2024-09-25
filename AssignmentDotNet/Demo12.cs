6b85468244896ebcb2a2ab30375cf331cc1a3dc1
Datta sonalkar <Dell@DESKTOP-TR6VTDT>
2024-09-25 18:11:23 +0530

    add new class

---
+++ AssignmentDotNet/Demo12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentDotNet
+{
+    internal class Demo12
+    {
+       static void Main()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
